ce53d5d03c34eeb9757732e4d8ee8c64adbf7216
ravichandru16 <r16rockers@gmail.com>
2026-02-20 12:40:47 +0530

    Amazon.in

---
+++ src/test/java/tasks_01/Amazon_Task.java
@@ -0,0 +1,27 @@
+package tasks_01;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class Amazon_Task {
+
+	@Test
+	public void AmazonTask1() throws InterruptedException
+	{
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		Actions act= new Actions(driver);
+		act.moveToElement(driver.findElement(By.xpath("//span[@class='icp-nav-link-inner']"))).perform();
+		act.moveToElement(driver.findElement(By.xpath("//span[@class='nav-text' and contains(.,'MR')]/i"))).click().perform();
+		
+		Thread.sleep(3000);
+		driver.quit();
+	}
+}
